60e3ccc6dcf5c1923a616d6fd73eff4ae78a239e
surajkr12444-prog <surajkr12444@gmail.com>
2026-09-19 16:52:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(async ({ mode }) => {
   }
 
   return {
-    base: '/udaan-final-frontend/',
+  base: '/entrepreneur/',
     plugins,
     envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
     define: processEnvDefines,
